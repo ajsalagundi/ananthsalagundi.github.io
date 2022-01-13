1170f487ee248ec753fefdf6333e5ec587624c9f
Ananth Salagundi <ananthsalagundi@Jays-MacBook-Pro.local>
2022-01-13 10:10:46 -0600

    changing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,9 @@ function Navbar() {
 	return (
 		<div className="text-orange-200 h-full w-1/3">
 			<h1 className="w-full pl-20 pt-24">AS</h1>
-			<ul className="pl-48 pt-32">
-				<li className="">
-					<span>Main</span>
-				</li>
-				<li className="">
-					<span>About</span>
-				</li>
-				<li className="">
-					<span>Projects</span>
-				</li>
-			</ul>
+			<p className="pl-48 pt-32">
+				
+			</p>
 		</div>
 	);
 }
